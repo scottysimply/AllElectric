2357f7da7e761e5047207167c069ee673dc0ed58
Joey Powers <155122741+Joey-Powers13@users.noreply.github.com>
2024-12-02 09:12:09 -0600

    DB connection for localhost (cpanel server not accessible)

--- products/productDb.php
+++ products/productDb.php
@@ -1,3 +1,13 @@
 <?php
-    $db = new PDO("", "", "");
+    $host = '127.0.0.1'; 
+    $dbname = 'ozarktechwebdev_all_electric';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
 ?>
