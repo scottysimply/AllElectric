3ec14c2a39d089c5a730da3ecdefe51424331b1e
Joey Powers <155122741+Joey-Powers13@users.noreply.github.com>
2024-12-04 09:21:18 -0600

    Fixed changes to show products

--- products/showProducts.php
+++ products/showProducts.php
@@ -1,5 +1,5 @@
 <?php
-    include 'productDb.php';
+include 'productDb.php';
     $product_results = [];
     try {
         $stmt = $pdo->query('SELECT * FROM products');
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Products</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
