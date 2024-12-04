9e14d176e12de51b542f741d7c91489d548d18ec
scottsimply <scottistriggered@gmail.com>
2024-12-04 10:09:15 -0600

    Completed the functionality of the add product page. Requires styling

--- products/addProduct.php
+++ products/addProduct.php
@@ -2,8 +2,8 @@
     include_once 'productDb.php';
     $product_type_query = "SELECT * FROM product_type";
     $brand_query = "SELECT * FROM brands";
-    $product_type_results = $db->query($product_type_query)->fetchAll(PDO::FETCH_ASSOC);
-    $brand_reults = $db->query($brand_query)->fetchAll();
+    $product_type_results = $pdo->query($product_type_query)->fetchAll(PDO::FETCH_ASSOC);
+    $brand_results = $pdo->query($brand_query)->fetchAll();
     $product_uses = ['Commercial', 'Residential'];
 ?>
 
@@ -18,22 +18,35 @@
     <?php
         function showErrorBanner(string $message) {
             // TODO: Show error message banner
+            echo $message;
         }
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             try {
                 // Show error if the dated image exists on the server
                 $uploaded_img = $_FILES['productImage'];
-                $output_image_name = $uploaded_img['tmpname'] . date("mdY-His") . ".png";
+                $output_image_name = pathinfo($uploaded_img['name'], PATHINFO_FILENAME) . date("mdY-His") . ".png";
                 if (file_exists("./images/" . $output_image_name)) {
                     showErrorBanner("The file already exists on the web server!");
                     return;
                 }
                 // This abomination uploads the image
-                imagepng(imagecreatefromstring(file_get_contents($uploaded_img['tmpname'])), "./images/" . $output_image_name);
+                if (!imagepng(imagecreatefromstring(file_get_contents($uploaded_img['tmp_name'])), './images/' . $output_image_name)) {
+                    showErrorBanner("The file could not be uploaded");
+                    return; 
+                }
                 
-                $unprepared_query = "INSERT INTO products VALUES = (:brand_id, :product_type_id, :product_name, :product_desc, :prod_intended_use, :img_url)";
-
-
+                $unprepared_query = "INSERT INTO products (brand_id, product_type_id, product_name, product_desc, product_intended_use, product_image)VALUES (:brand_id, :product_type_id, :product_name, :product_desc, :product_intended_use, :img_url)";
+                $prepared_statement = $pdo->prepare($unprepared_query);
+                $prepared_statement->execute([':brand_id' => $_POST['brand'], ':product_type_id' => $_POST['productType'], ':product_name' => $_POST['productName'], ':product_desc' => $_POST['productType'], ':product_intended_use' => $_POST['productUse'], ':img_url' => "./images/$output_image_name"]);
+        
+                // if control got to here, then it executed successfully. unset post values
+                unset($_POST['brand']);
+                unset($_POST['productType']);
+                unset($_POST['productName']);
+                unset($_POST['productDesc']);
+                unset($_POST['productUse']);
+                unset($_FILES['productImage']);
+                showErrorBanner("Product successfully added!");
             }
             catch (Exception $ex) {
                 // NEXT TIME: Add warning banner on error
@@ -43,18 +56,19 @@
         }
 
     ?>
-    <form method="POST" action=".">
+    <form method="POST" enctype="multipart/form-data">
+
         <select name="brand" id="brand">
             <?php
-                foreach ($brand_reults as $brand) {
-                    echo '<option value=' . $brand['brand_id'] . '></option>';
+                foreach ($brand_results as $brand) {
+                    echo'<option value=' . $brand['brand_id'] . '>' . $brand['brand_name'] . '</option>';
                 }
             ?>
         </select>
         <select name="productType" id="product-type">
             <?php
-                foreach ($product_type_reults as $product_type) {
-                    echo '<option value=' . $product_type['product_type_id'] . '></option>';
+                foreach ($product_type_results as $product_type) {
+                    echo'<option value=' . $product_type['product_type_id'] . '>' . $product_type['product_type_name'] . '</option>';
                 }
             ?>
         </select>
@@ -63,11 +77,12 @@
         <select name="productUse" id="product-use">
             <?php
                 foreach ($product_uses as $product_use) {
-                    echo 'option value=' . $product_use . '></option>';
+                    echo "<option value=$product_use>$product_use</option>";
                 }
             ?>
         </select>
         <input type="file" name="productImage" id="product-image">
+        <input type="submit" value="Add Product">
     </form>
 </body>
 </html>
